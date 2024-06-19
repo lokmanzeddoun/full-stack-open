4d69e5d1b634d882ca53ad1e98f5a783c84d8b91
lokmanzeddoun <l.zeddoun@esi-sba.dz>
2024-06-19 13:00:12 +0100

    front

--- Part3/index.js
+++ Part3/index.js
@@ -1,78 +1,110 @@
+import mongoose from "mongoose";
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-const app = express();
+import Person from "./models/Person.js";
 const PORT = process.env.PORT || 3000;
+const app = express();
 
 app.use(cors());
 morgan.token("body", (request) => JSON.stringify(request.body));
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(express.static("dist"));
-let persons = [
-	{
-		id: 1,
-		name: "Arto Hellas",
-		number: "040-123456",
-	},
-	{
-		id: 2,
-		name: "Ada Lovelace",
-		number: "39-44-5323523",
-	},
-	{
-		id: 3,
-		name: "Dan Abramov",
-		number: "12-43-234345",
-	},
-	{
-		id: 4,
-		name: "Mary Poppendieck",
-		number: "39-23-6423122",
-	},
-];
-app.get("/api/persons", (req, res, next) => {
-	res.status(200).json(persons);
-});//full-stack-open-2yxj.onrender.com
-https: app.get("/info", (req, res, next) => {
+app.get("/api/persons", async (req, res, next) => {
+	try {
+		const persons = await Person.find();
+		res.status(200).json(persons);
+	} catch (error) {
+		next(error);
+	}
+});
+app.get("/info", async (req, res, next) => {
 	const currentDate = new Date();
-	const htmlContent = `<p>PhoneBook has info for ${persons.length}  people </p>
-<p>${currentDate}</p>`;
-	res.status(200).send(htmlContent);
+	try {
+		const persons = await Person.find();
+
+		const htmlContent = `<p>PhoneBook has info for ${persons.length}  people </p>
+	<p>${currentDate}</p>`;
+		res.status(200).send(htmlContent);
+	} catch (error) {
+		next(error);
+	}
 });
-app.get("/api/persons/:id", (req, res, next) => {
-	const p = persons.find((item) => item.id === +req.params.id);
-	res.status(200).json(p);
+app.get("/api/persons/:id", async (req, res, next) => {
+	try {
+		const p = await Person.findById(req.params.id);
+		res.status(200).json(p);
+	} catch (error) {
+		next(error);
+	}
 });
-app.delete("/api/persons/:id", (req, res, next) => {
-	const p = persons.filter((item) => item.id !== +req.params.id);
-	persons = p;
-	res.status(204).send();
+app.delete("/api/persons/:id", async (req, res, next) => {
+	try {
+		await Person.findByIdAndDelete(req.params.id);
+		res.status(204).send();
+	} catch (error) {
+		next(error);
+	}
 });
 const postMorgan = morgan(
 	":method :url :status :res[content-length] - :response-time ms :body"
 );
-app.post("/api/persons", postMorgan, (req, res, next) => {
-	const generatedId = Math.round(Math.random() * 6000);
+app.post("/api/persons", postMorgan, async (req, res, next) => {
 	const { name, number } = req.body;
 	if (!name || !number) {
 		return res
 			.status(400)
 			.json({ error: "You must include name and number in the request" });
-	} else if (persons.some((p) => p.name === name)) {
+	} else if (await Person.findOne({ name })) {
+		console.log("what");
 		return res.status(400).json({ error: "name must be unique" });
 	}
-	const newPerson = {
-		id: generatedId,
-		name,
-		number,
-	};
-	persons = persons.concat(newPerson);
-	res.status(200).json(newPerson);
+	try {
+		const result = await Person.create(req.body);
+		res.status(200).json(result);
+	} catch (error) {
+		next(error);
+	}
 });
+app.put("/api/persons/:id", async (req, res, next) => {
+	try {
+		const person = await Person.findByIdAndUpdate(
+			req.params.id,
+			{
+				name: req.body.name,
+				number: req.body.number,
+			},
+			{ new: true, runValidators: true }
+		);
+		res.status(200).json(person);
+	} catch (error) {
+		next(error);
+	}
+});
+// for handling the Unknown Endpoint
 app.all("*", (req, res, next) => {
 	next(new Error(`Can't find this route: ${req.originalUrl}`));
 });
+// errorHandler
+const errorHandler = (err, req, res, next) => {
+	console.log(err.message);
+	if (err.name === "CastError") {
+		return res.status(400).send({ error: "mal formatted id" });
+	} else if (err.name === "ValidationError") {
+		return res.status(400).send({ error: err.message });
+	}
+	next(err);
+};
+app.use(errorHandler);
 app.listen(PORT, () => {
-	console.log(`Server is Listening On port ${PORT}`);
+	mongoose
+		.connect(process.env.MONGO_URI)
+		.then((result) => {
+			console.log(`Connected to Mongodb Atlas`);
+			console.log(`Server is Listening On port ${PORT}`);
+		})
+		.catch((err) => {
+			console.log(`error connecting to mongoDB ${err.message}`);
+		});
 });
---
+++ Part3/models/Person.js
@@ -0,0 +1,24 @@
+import mongoose, { Types } from "mongoose";
+
+mongoose.set("strictQuery", false);
+
+const personSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		minLength: [3, "Must be at least 3 , got {VALUE}"],
+		required: true,
+	},
+	number: {
+		type: String,
+		minLength: [8, "Must be at least 8 , got {VALUE}"],
+		validate: {
+			validator: (v) => {
+				return /^\d{2,3}-\d{7,}/.test(v);
+			},
+			message: (props) => `${props.value} is not a valid phone number !`,
+		},
+		required: true,
+	},
+});
+
+export default mongoose.model("Person", personSchema);
