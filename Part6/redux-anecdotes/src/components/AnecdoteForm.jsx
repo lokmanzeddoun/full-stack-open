79899802cb81df466ae07970107d2a8109d75f80
lokmanzeddoun <l.zeddoun@esi-sba.dz>
2024-07-25 19:23:48 +0100

    6.6 6.8

---
+++ Part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,23 @@
+import { createAnecdote } from "../reducers/anecdoteReducer";
+import { useDispatch } from "react-redux";
+const AnecdoteForm = () => {
+	const dispatch = useDispatch();
+	const submitNewNote = (e) => {
+		e.preventDefault();
+		dispatch(createAnecdote(e.target.anecdotes.value));
+		e.target.anecdotes.value = "";
+	};
+	return (
+		<div>
+			<h2>create new</h2>
+			<form onSubmit={submitNewNote}>
+				<div>
+					<input name="anecdotes" />
+				</div>
+				<button>create</button>
+			</form>
+		</div>
+	);
+};
+
+export default AnecdoteForm;
