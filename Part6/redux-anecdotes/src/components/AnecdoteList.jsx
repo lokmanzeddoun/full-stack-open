b45f48e75de6da799050f13e01ac34058a69eef8
lokmanzeddoun <l.zeddoun@esi-sba.dz>
2024-07-25 20:03:53 +0100

    6.9

--- Part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ Part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,13 +1,20 @@
 import { useSelector, useDispatch } from "react-redux";
 import { createVote } from "../reducers/anecdoteReducer";
 const AnecdoteList = () => {
-	const anecdotes = useSelector((state) =>
-		state.sort((a, b) => b.votes - a.votes)
-	);
+	const anecdotes = useSelector((state) => {
+		
+		if (state.filter === null) {
+			return state.anecdotes.sort((a, b) => b.votes - a.votes);
+		}
+		return state.anecdotes
+			.filter((anecdote) =>
+				anecdote.content.toLowerCase().includes(state.filter.toLowerCase())
+			)
+			.sort((a, b) => b.votes - a.votes);
+	});
 	const dispatch = useDispatch();
 
 	const vote = (id) => {
-		console.log("vote", id);
 		dispatch(createVote(id));
 	};
 	return (
---
+++ Part6/redux-anecdotes/src/components/Filter.jsx
@@ -0,0 +1,20 @@
+import { useDispatch } from "react-redux";
+import { turnFilter } from "../reducers/filterReducer";
+const Filter = () => {
+	const dispatch = useDispatch();
+	const handleChange = (event) => {
+		// input-field value is in variable event.target.value
+		dispatch(turnFilter(event.target.value));
+	};
+	const style = {
+		marginBottom: 10,
+	};
+
+	return (
+		<div style={style}>
+			filter <input onChange={handleChange} />
+		</div>
+	);
+};
+
+export default Filter;
