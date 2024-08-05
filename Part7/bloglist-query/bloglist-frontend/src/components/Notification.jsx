4d080e21533b026413c383d76cc219037e9d1dcb
lokmanzeddoun <l.zeddoun@esi-sba.dz>
2024-08-05 13:08:26 +0100

    finish part 7

--- Part7/bloglist-query/bloglist-frontend/src/components/Notification.jsx
+++
@@ -1,9 +0,0 @@
-const Notification = ({ message,error }) => {
-	return message === null ? null : (
-		<div className="msg" style={{ color: error ? "red" : "green" }}>
-			{message}
-		</div>
-	);
-};
-
-export default Notification;
